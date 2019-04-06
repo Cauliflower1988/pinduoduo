d25ed19608b46daae77901cb51e9f89fcd094942
wpcfan <peng.wang@outlook.com>
2019-04-06 21:54:47 +0800

    chap 4.10 路由事件的 rx 表述

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Router, NavigationEnd } from '@angular/router';
 import { TabItem, DialogService } from './shared';
-import { filter } from 'rxjs/operators';
+import { filter, map } from 'rxjs/operators';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -10,9 +11,16 @@ import { filter } from 'rxjs/operators';
 })
 export class AppComponent implements OnInit {
   constructor(private router: Router, private dialogService: DialogService) {}
-  selectedTabName = 'home';
+  selectedIndex$: Observable<number>;
   ngOnInit(): void {
-    this.routeEvent(this.router);
+    this.selectedIndex$ = this.router.events.pipe(
+      filter(ev => ev instanceof NavigationEnd),
+      map((ev: NavigationEnd) => {
+        const arr = ev.url.split('/');
+        return arr.length > 1 ? arr[1] : 'home';
+      }),
+      map(tab => this.getSelectedIndex(tab))
+    );
   }
   handleTabSelect(tab: TabItem) {
     this.router.navigate([tab.link]);
@@ -20,24 +28,14 @@ export class AppComponent implements OnInit {
   removeDialog() {
     this.dialogService.close();
   }
-  routeEvent(router: Router) {
-    router.events.subscribe(e => {
-      if (e instanceof NavigationEnd) {
-        const arr = e.url.split('/');
-        if (arr.length > 1) {
-          this.selectedTabName = arr[1];
-        }
-      }
-    });
-  }
-  get selectedIndex() {
-    return this.selectedTabName === 'home'
+  getSelectedIndex(tab: string) {
+    return tab === 'home'
       ? 0
-      : this.selectedTabName === 'recommend'
+      : tab === 'recommend'
       ? 1
-      : this.selectedTabName === 'category'
+      : tab === 'category'
       ? 2
-      : this.selectedTabName === 'chat'
+      : tab === 'chat'
       ? 3
       : 4;
   }
